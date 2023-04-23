41ce5e37b266a5314397e3b70f4211c3460a43cd
ZkunCry <aksentev.03@mail.ru>
2023-04-23 12:20:52 +0500

    init repos

---
+++ MathparserCalc/MathParser.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathparserCalc
+{
+    class MathParser
+    {
+
+        private string source = "5*2";
+        private int symbol, indexsrc = 0;
+        private void GetSymbol()
+        {
+            if (indexsrc + 1 <= source.Length)
+            {
+                symbol = source[indexsrc];
+                indexsrc++;
+            }
+            else
+                symbol = '\0';
+
+        }
+        public void StartAnalize()
+        {
+            GetSymbol();
+            double result = MethodE();
+            Console.WriteLine($"Result operation {source} = {result}");
+        }
+        private double MethodE()
+        {
+            double x = MethodT();
+            while (symbol == '+' || symbol == '-')
+            {
+                char p = (char)symbol;
+                GetSymbol();
+                if (p == '+')
+                    x += MethodT();
+                else
+                    x -= MethodT();
+            }
+            return x;
+        }
+        private double MethodT()
+        {
+            double x = MethodM();
+            while (symbol == '*' || symbol == '/')
+            {
+                char p = (char)symbol;
+                GetSymbol();
+                if (p == '*')
+                    x *= MethodM();
+                else
+                    x /= MethodM();
+            }
+            return x;
+        }
+        private double MethodM()
+        {
+            double x = 0;
+            if (symbol == '(')
+            {
+                GetSymbol();
+                x = MethodE();
+                GetSymbol();
+            }
+            else
+            {
+                if (symbol == '-')
+                {
+                    GetSymbol();
+                    x = -MethodM();
+                }
+                else
+                    if (symbol >= '0' && symbol <= '9')
+                    x = MethodC();
+            }
+            return x;
+        }
+        private double MethodC()
+        {
+            string x = "";
+
+            while (symbol >= '0' && symbol <= '9')
+            {
+                x += (char)symbol;
+                GetSymbol();
+                if (symbol == ',')
+                {
+                    x += ',';
+                    GetSymbol();
+                }
+
+
+            }
+            return double.Parse(x);
+        }
+
+    }
+}
---
+++ MathparserCalc/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MathparserCalc
+{
+    
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            MathParser parser = new();
+            parser.StartAnalize();
+        }
+        
+    }
+}
